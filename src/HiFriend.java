7292977b15de1d80aa3036f79e4747a20e0e7b4f
Kevin Hoang <kevin.hoang199@gmail.com>
2023-05-08 13:05:41 -0400

    uploading class files

---
+++ src/HiFriend.java
@@ -0,0 +1,8 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String a = "Hi ";
+        String b = "Kevin Hoang";
+        String c = a.concat(b);
+        System.out.println(c);
+    }
+}
---
+++ src/Vacation.java
@@ -0,0 +1,8 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("Places I'd like to visit:");
+        System.out.println("Japan");
+        System.out.println("Singapore");
+        System.out.println("Norway");
+    }
+}
